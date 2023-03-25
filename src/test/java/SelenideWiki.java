207ca0959228310e23146400b188f7abce4767e7
pianistka08 <pianistka08@mail.ru>
2023-03-25 12:27:18 +0300

    final

--- src/test/java/SelenideWiki.java
+++ src/test/java/SelenideWiki.java
@@ -24,8 +24,7 @@ public class SelenideWiki {
         Configuration.browser = "firefox";
     }
 
-    @Testssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9luxUaPeaYmOTxq+WOnxF81Cdh3Fu3ufhfwrCE8Fq/syDsIkZO5dnOxgnWSx6KSpxB7ZT4rciufsltBDiNtbAPyiwcGBwj8YDkS77oHGdl42lrpUKBRrgBBxntGLquXhcIeEOMJRERs0XatbVlhxIomTn39XlT3Nj2mWcb4q/ReGaXxCuBa/gXVyiY0T+188QdOiyocAemWGMW1PH5waDUPGQz6C29gYcj48vOaXjq3k9CevZeFUZk3U5AKiPjpwVhCJxpuWS2Pit7BxqkqfeKV1WHj/S8yhwXspGiwS7mZAd2wFVnFHZkbeRBHcd9bm5R1CR+AGJRkt7/y5wtZ0RCUl74LGwhxl9h6lvPKYY4ddEcqiQOqza31rP+GVSo9ZJ/9fgFhHLk3Kv6RYJG662hmdE2LtqA6dcRMViBt0+pRa7r7+YvqRNun3EfRTr5zz73LF/HGCGgfn//jbqBTbhmD/qcTaYA8h2fgvtohZSyRtB70EZb2Set8WjOpN/k3qEjUF+OU9TYYPZO82ws4XpXshXuRY2asrG/YGTqg4ToiXx3p6jix+Ek4CNW1XUoOlBYihJlkbgRwLpstQ9w61piB0r4drFJaDnCkRMaarD3lvLIks6yIyJw4Y7VXF9q1wvmG7agVOfMRadNPz/ZjMDhOSbTShQRr1I6/ZXmkRULw== pianistka08@mail.ru
-
+    @Test
     void testJunit5Class() {
         open("https://github.com/selenide/selenide");
         $("[id=wiki-tab]").click();
